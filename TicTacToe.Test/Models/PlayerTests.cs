ade8dceeb96729e42095a5cb4cdb72983b6bc202
Jie Sheng Teoh <teohjies@gmail.com>
2024-09-08 09:24:52 +0800

    test(player): set name

---
+++ TicTacToe.Test/Models/PlayerTests.cs
@@ -0,0 +1,26 @@
+﻿using TicTacToe.Models;
+
+namespace TicTacToe.Test.Models;
+
+[TestFixture]
+public class PlayerTests
+{
+    /// <summary>
+    /// Verifies that the <see cref="Player"/> class correctly stores the name when it is instantiated with a name.
+    /// </summary>
+    [Test]
+    public void Name_Set_WhenInstantiated_StoresNameCorrectly()
+    {
+        // Arrange
+        var name = "John Doe";
+
+        // Act
+        var player = new Player()
+        {
+            Name = name
+        };
+
+        // Assert
+        Assert.That(player.Name, Is.EqualTo(name));
+    }
+}
