b2ffb79d419b0b7aaf9927aeb58486d94024b80c
Jie Sheng Teoh <teohjies@zhulian.com.my>
2024-09-08 10:22:46 +0800

    feat(enum): hand

---
+++ TicTacToe/Models/Hand.cs
@@ -0,0 +1,17 @@
+﻿namespace TicTacToe.Models;
+
+/// <summary>
+/// Represents the possible values for a player's hand in the TicTacToe game.
+/// </summary>
+public enum Hand
+{
+    /// <summary>
+    /// Represents the 'X' hand, typically used by one of the players.
+    /// </summary>
+    X,
+
+    /// <summary>
+    /// Represents the 'O' hand, typically used by the other player.
+    /// </summary>
+    O
+}
